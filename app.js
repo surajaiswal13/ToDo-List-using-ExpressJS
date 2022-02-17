16a352d47390c493b484763f3c7292884b45dac7
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-17 20:48:44 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+
+  var today = new Date();
+  // var currentDay = today.getDay();
+  // var day = "";
+  //
+  // // 0 is Sunday and 6 is Saturday
+  //
+  // if(today.getDay() === 6 || today.getDay() === 0){
+  //   // res.write("<h1>Yay it's the weekend!</h1>");
+  //   // res.send();
+  //
+  //   day = "Weekend"
+  //
+  // }else {
+  //   // res.write("<h1>It is not the Weekend</h1>")
+  //   // res.write("<h1>Boo, It is a weekday You need to work!</h1>");
+  //   // res.send();
+  //   // res.sendFile(__dirname + "/index.html")
+  //
+  //   day = "Weekday"
+  //
+  // }
+  //
+  // switch(currentDay) {
+  //   case 0:
+  //     day = "Sunday";
+  //     break;
+  //
+  //   case 1:
+  //     day = "Monday";
+  //     break;
+  //
+  //   case 2:
+  //     day = "Tuesday";
+  //     break;
+  //
+  //   case 3:
+  //     day = "Wednesday";
+  //     break;
+  //
+  //   case 4:
+  //     day = "Thursday";
+  //     break;
+  //
+  //   case 5:
+  //     day = "Friday";
+  //     break;
+  //
+  //   case 6:
+  //     day = "Saturday";
+  //     break;
+  //
+  //   default:
+  //     console.log("This is not a Day : " +currentDay)
+  // }
+  //
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  // render is ejs method it will render context to list.ejs
+  res.render("list", {kindOfDay: day});
+
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
